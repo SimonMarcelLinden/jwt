21d991dc95ec10e6c4eefb801608e798eefde80e
Simon Marcel Linden <linden@snerve.de>
2023-12-22 18:08:10 +0100

    Add JWTServiceProvider for integrating JWT with Laravel Auth
    This commit introduces JWTServiceProvider in the SimonMarcelLinden\JWT namespace.
    The provider extends Laravel's authentication system to support JWT by registering JWTGuard and JwtUserProvider.
    This setup allows the Laravel application to handle JWT-based authentication seamlessly, enhancing the security and flexibility of the authentication process.

--- src/JWTServiceProvider.php
+++ src/JWTServiceProvider.php
@@ -10,14 +10,31 @@ use SimonMarcelLinden\JWT\Auth\Providers\JwtUserProvider;
 use SimonMarcelLinden\JWT\Auth\Guards\JWTGuard;
 use SimonMarcelLinden\JWT\Console\Commands\JWTCommand;
 
+/**
+ * JWTServiceProvider is a service provider for integrating JWT authentication into Laravel.
+ * It registers JWTGuard and JwtUserProvider for handling authentication and user retrieval.
+ *
+ * @Author: Simon Marcel Linden
+ * @Version: 1.0.0
+ * @since: 1.0.0
+ *
+ */
 class JWTServiceProvider extends ServiceProvider {
 	/**
-	 * Bootstrap the application services.
-	 *
-	 * @return void
-	 */
+     * Bootstrap the application services.
+     * This method extends Laravel's Auth to use JWTGuard and JwtUserProvider.
+     *
+     * @return void
+     */
 	public function boot(): void {
+		Auth::extend('jwt', function ($app, $name, array $config) {
+			$key = env('JWT_SECRET');
+			return new JWTGuard(Auth::createUserProvider($config['provider']), $app['request'], $key);
+		});
 
+		Auth::provider('jwt', function ($app, array $config) {
+			return new JwtUserProvider();
+		});
 	}
 
 	/**
