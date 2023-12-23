96d0d48189e38f6a06964f1458460ca669c3e388
Simon Marcel Linden <linden@snerve.de>
2023-12-24 00:36:46 +0100

    Add Documentation for JWTKeyCommand for generating new JWT secret keys

--- src/Console/Commands/JWTKeyCommand.php
+++ src/Console/Commands/JWTKeyCommand.php
@@ -4,7 +4,14 @@ namespace SimonMarcelLinden\JWT\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
-
+/**
+ * The JWTKeyCommand class provides a console command for generating a new JWT secret key.
+*
+* @Author: Simon Marcel Linden
+* @Version: 1.0.0
+* @since: 1.0.0
+*
+*/
 class JWTKeyCommand extends Command {
 	/**
 	 * The name and signature of the console command.
@@ -29,15 +36,23 @@ class JWTKeyCommand extends Command {
 		parent::__construct();
 	}
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
+ 	/**
+     * Execute the console command.
+     * Generates and sets a new JWT secret key in the .env file.
+     *
+     * @return void
+     */
 	public function handle() {
 		$this->setEnvironmentValue('JWT_SECRET', Str::random(32));
 	}
 
+	/**
+     * Set a value in the .env file.
+     *
+     * @param string $key The key to set.
+     * @param string $value The value to set.
+     * @return void
+     */
 	protected function setEnvironmentValue($key, $value) {
 		$path = base_path('.env');
 		$env = file_get_contents($path);
